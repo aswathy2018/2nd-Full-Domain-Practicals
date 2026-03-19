ef5aeda3e2ed3e3a0c4226d61de46c9f69cb6de0
aswathy2018 <aswathysudhakaran3002@gmail.com>
2026-03-19 12:20:01 +0530

    React started

---
+++ React/Practicals/Child.jsx
@@ -0,0 +1,9 @@
+const Child = (props) =>{
+    return(
+        <div>
+            <h1>Hii: {props.name}</h1>
+        </div>
+    )
+}
+
+export default Child
---
+++ React/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Child from '../Practicals/Child'
+
+const App = () => {
+  return (
+    <>
+    <Child name = {"Aswathy"}/>
+    </>
+  )
+}
+
+export default App
