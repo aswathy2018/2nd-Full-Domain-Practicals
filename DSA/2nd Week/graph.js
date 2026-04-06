1aaf2b8686874d5f8739b680fbaa4bd236e67fd6
aswathy2018 <aswathysudhakaran3002@gmail.com>
2026-04-06 10:33:06 +0530

    Graph implementation completed

--- DSA/2nd Week/graph.js
+++ DSA/2nd Week/graph.js
@@ -31,6 +31,46 @@ class Graph{
             let curr = queue.shift()
 
             res.push(curr)
+
+            this.graph[curr].forEach(n => {
+                if(!visited.includes(n)){
+                    queue.push(n)
+                    visited.push(n)
+                }
+            });
         }
+        return res
+    }
+
+    DFS(start){
+        let stack = [start]
+        let visited = [start]
+        let res = []
+
+        while(stack.length){
+            let curr = stack.pop()
+
+            res.push(curr)
+
+            this.graph[curr].forEach(n=>{
+                if(!visited.includes(n)){
+                    stack.push(n)
+                    visited.push(n)
+                }
+            })
+        }
+        return res
     }
 }
+
+let val = new Graph()
+
+val.addE("a", "b")
+val.addE("c", "d")
+val.addE("f", "g")
+val.addE("h", "i")
+val.addE("j", "k")
+
+console.log("Graph: ", val.graph);
+console.log("BFS: ", val.BFS("a"));
+console.log("DFS", val.DFS("h"));
