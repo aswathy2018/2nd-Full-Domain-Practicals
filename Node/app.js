ac8eadb984f33a7a550ad518f9a1306c082f5395
aswathy2018 <aswathysudhakaran3002@gmail.com>
2026-04-10 18:40:11 +0530

    Added

---
+++ Node/app.js
@@ -0,0 +1,16 @@
+const {exec} = require("child_process")
+const { stdout } = require("process")
+
+exec("node child.js", (error, stdout, stderr) => {
+    if(error){
+        console.error(`Error: ${error.message}`)
+        return
+    }
+
+    if(stderr){
+        console.error(`Stderr: ${stderr}`)
+        return
+    }
+
+    console.log(`Output: \n${stdout}`);
+})
