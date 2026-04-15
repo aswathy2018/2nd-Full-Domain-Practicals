3b813a37a3d78d2a07ab968e5d09709a6defd79a
aswathy2018 <aswathysudhakaran3002@gmail.com>
2026-04-15 22:55:37 +0530

    Added

--- Node/demo.js
+++ Node/demo.js
@@ -1,16 +1,13 @@
 const express = require('express')
 const app = express()
+const path = require('path');
 
-app.get('/', (req, res)=>{
-    res.send("Home page")
-    
-    let val = req.query.id
-
-    if(!res){
-        res.status(404).send("Not found")
-
-        let 
-    }
+app.get('/', (req, res) => {
+    res.send("This is home page..")
 })
+
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, '404.html'));
+});
 
 app.listen(3002, ()=>console.log("Server connected.."))
