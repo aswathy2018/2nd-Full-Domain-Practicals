2fde23fb434da10701e2a1f5718ab98215d90011
aswathy2018 <aswathysudhakaran3002@gmail.com>
2026-05-05 19:09:49 +0530

    Added

--- Node/httpServer.js
+++ Node/httpServer.js
@@ -1,7 +1,7 @@
 const http = require('http')
 
 const server = http.createServer((req, res)=>{
-    res.end("Hiiiiiiiii")
+    res.end("Hii..")
 })
 
 server.listen(3001, ()=>console.log("Server connected.."))
